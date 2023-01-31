9f631e0ef9a310fc7864ca2d7c52dd49eecfda82
Cadpig <70630020+Cadpig@users.noreply.github.com>
2023-01-31 15:13:18 +0400

    Added main_dagster and main_prefect

---
+++ main_dagster.py
@@ -0,0 +1,26 @@
+from dagster import job, op
+import pandas as pd
+
+@op
+def load_file_to_db():
+    df = pd.read_csv("/Users/vladislavtihonov/Documents/University/Pipelines/original.csv")
+    return df
+
+@op
+def ctas(df):
+    new_col = []
+    for x in df['url']:
+        new_col.append(x.split("/",)[2])
+    df['domain_of_url'] = new_col
+    return df
+
+@op
+def copy_to_file(df):
+    df.to_csv('norm.csv.gz')
+    return 'norm.csv.gz'
+
+@job
+def serial():
+    df1 = load_file_to_db()
+    df2 = ctas(df1)
+    print(copy_to_file(df2))
---
+++ main_prefect.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from prefect import flow, task
+
+
+@task
+def load_file_to_db(file_name):
+    df = pd.read_csv(file_name)
+    return df
+
+@task
+def ctas(df):
+    new_col = []
+    for x in df['url']:
+        new_col.append(x.split("/",)[2])
+    df['domain_of_url'] = new_col
+    return df
+
+@task
+def copy_to_file(df):
+    df.to_csv('norm.csv.gz')
+    return 'norm.csv.gz'
+
+@flow
+def api_flow(file_name):
+    return copy_to_file(ctas(load_file_to_db(file_name)))
+
+print(api_flow("/Users/vladislavtihonov/Documents/University/Pipelines/original.csv"))
