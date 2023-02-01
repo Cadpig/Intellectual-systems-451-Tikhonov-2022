96ea29a53f13bcbf753aaeb4d2c87b66d9078a1f
Cadpig <70630020+Cadpig@users.noreply.github.com>
2023-02-01 20:13:31 +0400

    Added CLI features for prefect script

--- main_prefect.py
+++ main_prefect.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from prefect import flow, task
+import click
 
 
 @task
@@ -7,21 +8,25 @@ def load_file_to_db(file_name):
     df = pd.read_csv(file_name)
     return df
 
+
 @task
 def ctas(df):
     new_col = []
     for x in df['url']:
-        new_col.append(x.split("/",)[2])
+        new_col.append(x.split("/", )[2])
     df['domain_of_url'] = new_col
     return df
+
 
 @task
 def copy_to_file(df):
     df.to_csv('norm.csv.gz')
     return 'norm.csv.gz'
 
+
+@click.command()
+@click.argument('file_name')
 @flow
 def api_flow(file_name):
     return copy_to_file(ctas(load_file_to_db(file_name)))
 
-print(api_flow("/Users/vladislavtihonov/Documents/University/Pipelines/original.csv"))
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pipelines',
+    version='0.1.0',
+    py_modules=['main_prefect'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pipelines = main_prefect:api_flow',
+        ],
+    },
+)
